d692b3192bf3b2f6e4511ec38699f97b1a41e85b
Rayen Ogalde <rayen.ogalde@opencluster-tech.cl>
2026-08-26 14:04:47 -0400

    Conectar las paginas FNDR con el Apps Script

--- fndr/backend.js
+++ fndr/backend.js
@@ -1,2 +1,2 @@
 /* Direccion del Apps Script que recibe los datos. */
-window.FNDR_BACKEND = "";
+window.FNDR_BACKEND = "https://script.google.com/macros/s/AKfycbzEturTcxK-Pl2WqOADEa32j8fayTqd3s7BZqOev3G_uZnbDFgaB__nJUAl-gWvAgMp/exec";
